0e9075733507fd2d3fec66af8383df4fe57cb8a0
Franny <francisca.leonor.alejandra.c@gmail.com>
2021-07-05 13:13:06 -0400

    Add install|uninstall options

--- brightness-control.sh
+++ brightness-control.sh
@@ -17,8 +17,21 @@ BACKLIGHT_DRIVER_FOLDER="/sys/class/backlight/${DRIVER}";
 BRIGHTNESS_MAX=$(cat ${BACKLIGHT_DRIVER_FOLDER}/max_brightness)
 BRIGHTNESS_DEVICE="${BACKLIGHT_DRIVER_FOLDER}/brightness"
 
+function uninstall {
+    # CHECK ROOT PRIVILEDGES
+    if [[ "$UID" -ne 0 ]] ; then
+        echo "[Info] I need root priviledges to change brightness!"
+    else
+        echo -n "Uninstalling..."
+        exec_path=$(which ${THIS_FILE})
+        rm $exec_path
+        echo "[OK]"
+    fi
+}
+
 function install {
-    if [[ $(pwd) != ${INSTALLATION_PATH} ]] ; then
+    # INSTALL
+    if [[ $(dirname $0) != ${INSTALLATION_PATH} ]] ; then
         # CHECK ROOT PRIVILEDGES
         if [[ "$UID" -ne 0 ]] ; then
             echo "[Info] I need root priviledges to change brightness!"
@@ -29,17 +42,21 @@ function install {
             echo "[OK]"
             echo "Installed at: '$install_path'"
         fi
+    else
+        echo -e "[Info] Already installed!\n"
+        show_help
     fi
 }
 
 function show_help {
-    echo "Brightness Controll 0.1"
+    echo "Brightness Controll"
     echo -e "\nWrote by ${AUTHOR_NAME}"
     echo "Email: ${AUTHOR_EMAIL}"
-    echo -e "\nUsage: brightness-control [[i|install]|brightness percent(1~100)|[h|help]]"
-    echo "- number in range 1~100: Set brightness percent."
-    echo "- h|help:                Show help menu."
-    echo "- i|install:             Install this file at ${INSTALLATION_PATH} folder. (Only works once.)"
+    echo -e "\nUsage: brightness-control [--[install|uninstall]|INTEGER(1~100)|[h|help]]"
+    echo "INTEGER in range 1~100: Set brightness percent."
+    echo "-[h|help]:              Show help menu."
+    echo "--install:              Install at '${INSTALLATION_PATH}/' folder. (Only works once.)"
+    echo "--uninstall:            Uninstall from '${INSTALLATION_PATH}/'. (Only works once.)"
 }
 
 function setBrightness {
@@ -59,6 +76,7 @@ function setBrightness {
 # PROCESS OPTIONS
 case $1 in
     [0-9]*) setBrightness $1;;
-    "i"|"install"|"-i"|"--install") install;;
+    "--install") install;;
+    "--uninstall") uninstall;;
     "h"|"help"|"-h"|"--help"|*) show_help;;
 esac
